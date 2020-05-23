4f062b972e3a596f60cd642936e5cadb83f21df7
Sawaki <you@example.com>
2020-05-23 12:48:20 +0900

    Updates

---
+++ precache-manifest.ade3510ceb1d928b7c90a2a6f7df0b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cf17a459b36d20ae387591df3d7bb1",
+    "url": "/crud-pre-app-0523/index.html"
+  },
+  {
+    "revision": "113d1d17ed66a4c78428",
+    "url": "/crud-pre-app-0523/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "366058674dbe7d104845",
+    "url": "/crud-pre-app-0523/static/js/2.bc864809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crud-pre-app-0523/static/js/2.bc864809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113d1d17ed66a4c78428",
+    "url": "/crud-pre-app-0523/static/js/main.ac51cb86.chunk.js"
+  },
+  {
+    "revision": "2e3ebef11aac81c8bf83",
+    "url": "/crud-pre-app-0523/static/js/runtime-main.3b1729d9.js"
+  }
+]);
